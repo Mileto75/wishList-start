16726c66c5991e8a3be05307d97bde780b9ef9ef
Mileto Di Marco <mileto.di.marco@howest.be>
2021-02-22 14:36:59 +0100

    add css

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict"
+
+//globals vars
+
+
+window.addEventListener('load',Initialize);
+
+function Initialize()
+{
+    //bind the elements
+    
+    //add eventlisteners
+    
+    //set the theme
+    
+    //hide the errorbox
+    
+    //give focus to textbox
+}
+
+
+
+/**
+ * FUNCTIONS
+ */
